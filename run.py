07246203405ab6f3054e1100eaeed6484f923d44
Elisa <95719665+EliSacch@users.noreply.github.com>
2023-01-08 14:08:27 +0000

    Add logic to retrieve data from json file

--- run.py
+++ run.py
@@ -1,15 +1,19 @@
 import os
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    data = []
+    with open("data/notes.json", "r") as json_data:
+        data = json.load(json_data)
+    return render_template("index.html", page_title="Dashboard", notes=data)
 
 @app.route("/add")
 def add():
-    return render_template("add.html")
+    return render_template("add.html", page_title="Add new")
     
 
 if __name__ == "__main__":
--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
     
+    /**
+     * Options button
+     */
     $('#options-toggler').click( () => {
         $('#options').slideToggle();
         $('#options-toggler').attr('aria-expanded', (i, attr) => {
@@ -10,4 +13,23 @@ $(document).ready(function(){
             }
         })
     });
+
+
+    /**
+     * Edit note button
+     */
+    $('.edit-btn').click( () => {
+        console.log('edit')
+    });
+
+
+    /**
+     * 
+     * Delete note button
+     */
+    $('.delete-btn').click( () => {
+        console.log('delete')
+    });
+
+
 });
